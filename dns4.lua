656de1acb87c768260d79bfacd8c294b63f5af67
troiiiiijannnnn <137407362+troiiiiijannnnn@users.noreply.github.com>
2025-07-09 15:41:40 +0800

    Create dns4.lua

---
+++ dns4.lua
@@ -0,0 +1,3 @@
+local p = game:GetService("Players").LocalPlayer
+local e = p.Name
+local eng = game:GetService("Workspace"):WaitForChild("Jeepnies"):WaitForChild(e):WaitForChild("PartsStatusValues")Destroy()
